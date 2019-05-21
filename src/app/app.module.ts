4d545f9fb634c6d3e3c3364fc2634bed627a2f76
hfakouri <fakoori@gmail.com>
2019-05-21 14:39:35 -0400

    Added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularCliComponent } from './components/angular-cli/angular-cli.component';
+import { SimpleComponent } from './components/simple/simple.component';
+import { AttributeSelectorComponent } from './components/attribute-selector/attribute-selector.component';
+import { ClassSelectorComponent } from './components/class-selector/class-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularCliComponent,
+    SimpleComponent,
+    AttributeSelectorComponent,
+    ClassSelectorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/simple/simple.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-simple',
+    template: `
+        <div class="card mt-3">
+            <div class="card-header">
+                Simple
+            </div>
+            <div class="card-body">
+                <p class="card-text">A simple component that has been created manually.
+                    <span class="redtext">Inline style directly from component class.</span>
+                </p>
+            </div>
+        </div>
+    `,
+    styles: [`
+    .redtext {
+        color: red;
+    }`]
+})
+
+export class SimpleComponent {
+    constructor() {
+    }
+}
