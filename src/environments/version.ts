29b1b43e55755e24a16df2e7722a830d62f43a7d
Hossein Fakouri <hfakouri@targray.com>
2019-08-14 10:48:48 -0400

    Updated the screenshot

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "d15963c",
-    "hash": "d15963c",
+    "dirty": true,
+    "raw": "f67ea98-dirty",
+    "hash": "f67ea98",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "d15963c",
+    "suffix": "f67ea98-dirty",
     "semverString": null,
     "version": "0.0.0"
 };
